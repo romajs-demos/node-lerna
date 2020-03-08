589d50902c483042c79b7e90ef58394f9da51c7a
Romualdo Santos <romajslit@gmail.com>
2020-03-08 16:24:58 -0300

    Adding app

---
+++ packages/app/index.js
@@ -0,0 +1,7 @@
+const bar = require("@romajs-demos/node-lerna-bar");
+const baz = require("@romajs-demos/node-lerna-baz");
+const foo = require("@romajs-demos/node-lerna-foo");
+
+bar();
+baz();
+foo();
