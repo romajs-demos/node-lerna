806cd994c7a117fa90e6bb68594f4ec8c79f886c
Romualdo Santos <romajslit@gmail.com>
2020-03-08 16:35:50 -0300

    Adding app dependencies

--- packages/app/index.js
+++ packages/app/index.js
@@ -2,6 +2,6 @@ const bar = require("@romajs-demos/node-lerna-bar");
 const baz = require("@romajs-demos/node-lerna-baz");
 const foo = require("@romajs-demos/node-lerna-foo");
 
-bar();
-baz();
-foo();
+bar.hello();
+baz.hello();
+foo.hello();
